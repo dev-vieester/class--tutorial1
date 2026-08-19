2aa9764491f59850854fd93df4ac746807aff26c
Adeniyi Victor <adeniyivictor03@gmail.com>
2026-08-19 21:59:53 +0100

    Initial commit

---
+++ awele/tutorial1.py
@@ -0,0 +1,6 @@
+print(2 + 3)
+print(9 * 8)
+print(20 / 2)
+
+#cd to change directory
+#cd .. go back to previous directory
---
+++ tutorial3.py
@@ -0,0 +1,37 @@
+#4 basic data
+
+#Integar - 123457890 number(Whole number)
+#Float - decimal number(0.1, 1.2, 2.2)(Decimal)
+#String - Testing1234@, Awele, Jennifer, Precious, Lagos, Ilorin, Nigerial
+#boolean - True and False
+
+age = 10
+cgpa = 3.2
+country= "Togo"
+is_boy = False
+
+first_name= ("Precious is a girl"
+             "She is a girl"
+             "She like coding")
+last_name= "Arinola"
+above_teen = True
+
+
+#Variable
+pi= 3.14
+r = 80
+
+print(pi * (r ** 2))
+current_weather= 2.3
+# name = 'Segun'
+# print(current_weather + ' is a boy')
+# print(name + ' is 2 years old')
+# print(name + ' lives in Ilorin')
+# print(name + ' likes coding')
+# print(name + ' like eating and sleeping')
+# name = 'Victor'
+# print('  ')
+# print(name + ' is a boy')
+# print(name + ' is 2 years old')
+# print(name + ' lives in Ilorin')
+# print(name + ' likes coding')

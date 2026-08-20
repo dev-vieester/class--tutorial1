7612493d42d11ff0ec4f5db1091a5e1de1a83cf6
Adeniyi Victor <adeniyivictor03@gmail.com>
2026-08-20 22:01:03 +0100

    Second Class

---
+++ tutorial4.py
@@ -0,0 +1,34 @@
+#Class and Object
+#Classification of Living things
+# Antropod(Class) - Insects - Properties and Behavior - 8 legs, crawl
+# and Mammal(Class) - Human Begin - Properties and Behavior - 2 legs, walk
+
+#Class and Object
+#String(class) - 'Victor'(Object) - Property and Behavior
+#int(class) - 40, 50(object) - properties and behavior
+#Python is dynamic language
+
+variable = int(40)
+print(type(variable))
+variable = str('Victor')
+print(type(variable))
+
+print(variable + variable)
+#VictorVictor
+
+
+# first_name = str("Segun")
+# age= 20
+# new_age = int(30)
+# cgpa = 3.9
+# new_cgpa = float(cgpa)
+# is_boy = bool(False)
+# is_girl = True
+
+# print(name)
+# print(type(name))
+# print(isinstance(name, int))
+#
+# print(name.lower())
+# print(name.upper())
+# print(age + age)
---
+++ tutorial5_string.py
@@ -0,0 +1,76 @@
+# I'm
+sentence = "I'm a boy"
+
+greeting = "Hello"
+
+a = """
+Hello!!! How are you?
+Am just greeting
+Bye bye"""
+
+# print(a[-1])
+
+
+# print(greeting + "Victor")
+# print(a)
+
+# name = "Victor"
+# print(len(name)) #Get length of a String
+# print(name[len(name) - 1])
+#0123456789 - Indexing
+
+#Check for a String
+txt = "The best things in life are free!"
+# print("free" in txt)
+
+# print("book" not in txt)
+
+#Slicing
+name = "Victor"
+# print(name[0:3]) #Vic
+# print(name[2:]) #ctor
+# print(name[2:-1]) #cto
+
+
+#Modify
+# a = 'jennifer'
+# a = a.upper()
+# print(a.lower())
+# print(a)
+
+# a = " Hello, World! " #Removes whitespace
+# print(a)
+# print(a.strip())
+
+#Replace a string
+# name = "Bola"
+# print(name.replace("a", "u"))
+
+#Split
+# a = "Hello World"
+# print(a.split(' '))
+
+#concatenation
+# first_name = "Jennifer"
+# last_name = "Queen"
+# full_name = first_name + " " + last_name
+#
+# print(full_name)
+
+#f- format
+first_name = "Awele"
+last_name = "Queen"
+gender = "female"
+state = "Lagos"
+price = 12.98938768768
+age = 40
+full_name = f"{first_name} {last_name} is a {gender} and she lives in {state} and she sells bags for {price:.3f} and she is {age} years old"
+print(full_name)
+
+#Escape Character
+txt = "We are the so-called "Vikings" from the north."
+txt = "We are the so-called \"Vikings\" \nfrom the north."
+sentence = 'I\'m a boy'
+
+
+

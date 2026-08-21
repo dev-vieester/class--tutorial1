341b6c2dabdf36e238bfb232ac3e7836d8801fd8
Adeniyi Victor <adeniyivictor03@gmail.com>
2026-08-21 22:08:16 +0100

    Number and Maths tutorial

---
+++ tutorial6_number.py
@@ -0,0 +1,80 @@
+#Numbers are divided into 3 types float integer and complex
+#Class - Sub-class - Parent - Child i, my brother and my sister
+# print(1 + 1.0)
+# print(1.0 + 1.0)
+# print('Precious' + 1) -- Error
+
+age = 10 #integer
+new_age = int(20)
+name = "Awele"
+value ='6'
+
+cgpa = 2.9 #float
+new_cgpa = float(3.9)
+
+c= 1j #complex number
+complex_number = complex(9j)
+#
+# v = 3.47e4
+# print(v)
+
+# print(type(age))
+# print(type(new_age))
+# print(type(c))
+# print(type(value))
+
+# age = float(age)
+# name = float(name)
+# value = float(value)
+# print(type(value))
+# print(type(age))
+# print(type(name))
+
+#Modules - Library - Library are code written somewhere that can be reused
+
+# import random
+# fruits = ["Orange", "Mango", "Banana"]
+#
+# fruit = random.choice(fruits)
+# print(fruit)
+# number = random.randint(0, 101)
+# new_number = random.random()
+# num = random.uniform(10.5, 50.5)
+# print(num)
+# print(number)
+# print(new_number)
+
+# import math
+#
+# #Inbuilt math functions
+#
+# value = max(1,3,4,5,7)
+# value = min(1,3,4,5,7)
+# print(value)
+#
+# value = pow(4, 3)  # 4*4*4
+# value = abs(-3.9)
+# print(value)
+# print(value)
+# print(math.pi)
+# num = math.sqrt(60)
+# new_number = math.ceil(2.44)
+# print(new_number)
+
+# is_boy = True
+# is_girl = False
+is_boy = bool(0)
+print(is_boy)
+
+# print(10 > 9) # True
+# print(10 == 9) # False
+# print(10 < 9) # False
+# print(bool(''))
+# print(bool('U'))
+# print(bool(0))
+# print(bool(1))
+# print(bool("Hello"))
+# print(bool(15))
+
+
+

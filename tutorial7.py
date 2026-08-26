f0a42f45aa70e6ccc9831051aa60b34628c2a886
Adeniyi Victor <adeniyivictor03@gmail.com>
2026-08-26 21:54:37 +0100

    Tutorials added

---
+++ tutorial7.py
@@ -0,0 +1,65 @@
+#Create a simple calculator which will take input from user and perform arithmetic operations
+#Addition +
+#Substraction -
+#Division /
+#Multiplication *
+#Modulus %
+#Floor //
+#Arithmetic Operation
+# print(3 + 3) #integer
+# print(6 - 3) #integer
+# print(6 /3 ) #Float
+# print(3 * 3)
+# print(9 % 3) #3 Reminder of
+# print(10 // 3)
+# print(8 ** 2) Exponential
+#Logical Operation True or False <,> <=, >=, ==, and, or
+# print(10 < 9) #False
+# print(10 > 9) #True
+# print(10 <= 10) #True
+# print(10 >= 12) #False
+# print(100 != 101) #False !=(not equal to)
+#         #False         #True
+# print((10 > 10) and (10 > 3)) #False
+# print((10 > 10) or (10 > 3)) #True
+
+
+
+# age = "10"
+# print(type(age))
+# age = int(age)
+# print(type(age))
+
+#Taking Input from user
+#input 2 + 2 = 4
+# print("Hi!, Welcome to this application")
+# name = input("Please enter your name: ")
+# print(f"Ok!! {name}, you now perform calculation")
+# num1 = input(f"{name} Enter your first number: ") #Awele, 100, 10.2
+# num2 = input(f"{name} Enter your second number: ")
+# print(f"The difference between the two numbers is: {int(num1) - int(num2)}")
+
+# print(type(num1))
+# print(f"The inputted value is {num1}")
+#4
+# num = 3
+# #num = num + 4
+# num |= 2
+# print(num)
+
+#allocate a position of the memory where name is been store
+name = "Victor"
+print("i" not in name)
+
+#BODMAS - Bracket, Or, Division, Multiplication, Addition and Subtraction
+print((3*3) + 4/2 -6 *2)
+#9+2-12 = 11 - 12 (-1)
+
+#https://www.w3schools.com/python/python_operators_precedence.asp
+
+
+
+
+
+
+
---
+++ tutorial8.py
@@ -0,0 +1,26 @@
+#Arrays
+#List, sets, tuples, dictionary
+#index - position of an item in an iterable List, String, set
+#0,1,2,3,4,5,
+#1. A list is mutable and it is ordered
+# students = ["Awele", "Jennifer", "Victor", "Precious"] #List
+# print(students[3])
+# students[2] = "Segun"
+# print(students[2])
+
+# print(students)
+#list and String - iterable
+scores = [90, 30, 40, 30, 30, 50, 70]
+new_score = [88]
+scores += new_score
+print(len(scores))
+
+fruits = list(("Orange", "Mango", "Banana"))
+print(fruits)
+
+
+# print(scores)
+is_boys = [True, False, False, True]
+heights = [7.6, 4.5, 6.6, 7.8]
+
+

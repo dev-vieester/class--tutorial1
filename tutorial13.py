6bc58ce730a83b40c57d8379f777189a08be6236
Victor Adeniyi <adeniyivictor03@gmail.com>
2026-09-03 21:50:28 +0100

    Tutorial13

---
+++ tutorial13.py
@@ -0,0 +1,31 @@
+#Loop (Simply means go over an task)
+
+#While (When you how many times you will go over the loop) and
+#For (When you are certain of number of times you will go over a task)
+# infinite loop
+# value = 0
+#Break gets you out of the code
+#Continue skips and continue
+# while value < 10:
+#     value += 1
+#     print(value)
+#     if value == 5:
+#         continue
+#
+# value = 0
+# while value < 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+
+
+# fruits = ["orange", "mango", "banana", "apple", "pineapple"]
+# for fruit in fruits:
+#     print(fruit)
+#
+# print("done")
+
+for x in range(1, 11, 3):
+    pass
+
